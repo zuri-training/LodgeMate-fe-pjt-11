7737c8b6392d742e6dbdbd279a5999cdd135ef36
Morenike Oyewole <herroyalpianist@gmail.com>
2021-06-23 15:17:55 +0100

    CSS folder
    Added CSS files to CSS folder and refactored the header

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 $(".carousel").carousel({
   interval: 3000,
 });
+
+const toggle = () => {
+  const burger = document.querySelector("#ham");
+  const nav = document.querySelector(".nav-links");
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
+};
+toggle(); 
